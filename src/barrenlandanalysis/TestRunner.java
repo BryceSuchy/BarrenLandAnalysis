62ed1551a2dfc1f89de2c64783737562ff4045e6
Bryce Suchy <bryceman1290@hotmail.com>
2018-10-09 18:59:39 -0500

    Added print statement

--- src/barrenlandanalysis/TestRunner.java
+++ src/barrenlandanalysis/TestRunner.java
@@ -12,6 +12,12 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
 
     public static void main(String[] args) {
+        
+        System.out.println("Enter coordinates in the form of rectangles below." + "\n" + 
+                "These rectangles are defined in a string, which consists of four integers" + "\n" + 
+                "separated by single spaces, with no additional spaces in the string. " + "\n" +
+                "The first two integers are the coordinates of the bottom left corner in the given rectangle," + "\n" +
+                "and the last two integers are the coordinates of the top right corner. ");
 
         Result result = JUnitCore.runClasses(TestJUnit.class);
         for (Failure failure : result.getFailures()) {
